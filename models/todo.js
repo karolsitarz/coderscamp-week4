da816e060be414efc0530cf3d1eda151b4c827f1
JedrzejZygadlo <jedrekzygadlo@wp.pl>
2018-12-20 13:57:41 +0100

    Add todos endpoints, Todo model and todo validation

--- models/todo.js
+++ models/todo.js
@@ -3,13 +3,24 @@ const Joi = require('joi');
 
 const Todo = mongoose.model('todos',
   new mongoose.Schema({
-    // todo model
+    text: {
+      type: String,
+      required: true,
+      minlength : 1,
+      maxlength : 200
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'done']
+    }
   })
 );
 
 const validate = todo => {
   return Joi.validate(todo, {
-    // validation
+      text: Joi.string().required().min(1).max(200),
+      status: Joi.string().required().valid('active', 'done')
   });
 };
 
