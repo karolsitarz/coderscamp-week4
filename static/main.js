48f891f7f0f9c04f8f4e606871e92348cce0e668
Karol Sitarz <ka.sitarz@gmail.com>
2018-12-21 04:26:49 +0100

    add and delete actions

--- static/main.js
+++ static/main.js
@@ -15,19 +15,56 @@
     if (res.status !== 200) throw new Error(await res.json());
     else return res.json();
   };
+  const addTodo = ({ text, status, _id }) => {
+    document.getElementById('tasks').insertAdjacentHTML('beforeend',
+    `<div class="tasks-single" ${status === 'active' ? '' : 'data-done'} data-todoid=${_id}>
+    <div class="tasks-single-checkbox"></div>
+    <span class="tasks-single-desc">${text}</span>
+    <svg class="tasks-trash" viewBox="0 0 459 459">
+    <path d="M76.5,408c0,28.05,22.95,51,51,51h204c28.05,0,51-22.95,51-51V102h-306V408z M408,25.5h-89.25L293.25,0h-127.5l-25.5,25.5    H51v51h357V25.5z" />
+    </svg>
+    </div>`)
+  }
 
   const renderView = async () => {
     if (document.getElementById('user')) document.getElementById('user').remove();
     try {
       const tasks = await fetchAPI('/todos');
-      // for (let task of tasks) {
-
-      // }
-      console.log(tasks);
+      for (let task of tasks) {
+        addTodo(task);
+      }
     } catch (ex) {
       window.alert(ex.message);
     }
   };
+
+  document.forms.addTask.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    try {
+      const newTask = await fetchAPI('/todos', 'POST', {
+        text: this.text.value,
+      });
+      
+      addTodo(newTask);
+      this.text.value = '';
+    } catch (ex) {
+      window.alert(ex.message);
+    }
+  });
+
+  document.getElementById('tasks').addEventListener('click', async e => {
+    if (e.target && e.target.closest('.tasks-trash')) {
+      const id = e.target.closest('.tasks-single').dataset.todoid;
+
+      try {
+        await fetchAPI(`/todos/${id}`, 'DELETE');
+        
+        document.querySelector(`#tasks .tasks-single[data-todoid="${id}"]`).remove();
+      } catch (ex) {
+        window.alert(ex.message);
+      }
+    }
+  });
 
   document.forms.register.addEventListener('submit', async function (e) {
     e.preventDefault();
